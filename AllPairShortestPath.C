e6df495d5160e3bb34f9c1675a3c67d19a92bd01
Soham Bagal <sohamsbagal@gmail.com>
2025-10-27 00:45:02 +0530

    Update AllPairShortestPath.C

--- AllPairShortestPath.C
+++ AllPairShortestPath.C
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #define INF 99999
+#define MAX_V 20 
 
-void printSolution(int V, int dist[V][V]) {
+void printSolution(int V, int dist[MAX_V][MAX_V]) {
+    int i, j;
     printf("The following matrix shows the shortest distances between every pair of vertices:\n");
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
             if (dist[i][j] == INF)
                 printf("%7s", "INF");
             else
@@ -14,10 +16,11 @@ void printSolution(int V, int dist[V][V]) {
     }
 }
 
-void FloydWarshall(int V, int dist[V][V]) {
-    for (int k = 0; k < V; k++) {
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
+void FloydWarshall(int V, int dist[MAX_V][MAX_V]) {
+    int i, j, k; 
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
                 if (dist[i][k] != INF && dist[k][j] != INF &&
                     dist[i][k] + dist[k][j] < dist[i][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
@@ -30,20 +33,20 @@ void FloydWarshall(int V, int dist[V][V]) {
 
 int main() {
     int V;
+    int i, j;
+
     printf("Enter the number of vertices: ");
     scanf("%d", &V);
-
-    int dist[V][V];
+    int dist[MAX_V][MAX_V];
 
     printf("Enter the adjacency matrix:\n");
     printf("Use 99999 for infinity (no direct edge).\n");
 
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
             scanf("%d", &dist[i][j]);
         }
     }
     FloydWarshall(V, dist);
     return 0;
 }
-
